59354907fcd30eacc57151a8829e6901b2911e31
amankumar367 <kumaraman367@gmail.com>
2019-06-08 19:57:44 +0530

    UI part done and added read contact permission

--- app/src/main/java/com/dev/aman/android_contact_app/ContactDetailsActivity.java
+++ app/src/main/java/com/dev/aman/android_contact_app/ContactDetailsActivity.java
@@ -1,13 +1,14 @@
 package com.dev.aman.android_contact_app;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class ContactDetailsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_contact_details);
     }
 }
---
+++ app/src/main/java/com/dev/aman/android_contact_app/ContactListActivity.java
@@ -0,0 +1,13 @@
+package com.dev.aman.android_contact_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ContactListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contact_list);
+    }
+}
