f4a58c1ce7c9de9d40a12db79a0e0fa42e04917d
amankumar367 <kumaraman367@gmail.com>
2019-06-08 17:35:09 +0530

    Organized Dependencies

--- app/src/main/java/com/dev/aman/android_contact_app/MainActivity.java
+++ app/src/main/java/com/dev/aman/android_contact_app/MainActivity.java
@@ -1,6 +1,6 @@
 package com.dev.aman.android_contact_app;
 
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
